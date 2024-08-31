b2b537f5394d820f39198a57d4c6a9a51026e698
Denis-Albertini <denis.s.albertini@gmail.com>
2024-08-31 15:34:57 -0300

    add atividade-js-2

---
+++ atividade-js-2/js/library.js
@@ -0,0 +1,89 @@
+const books = [];
+
+function addBook() {
+  const book = {
+    title: $("#title").val(),
+    description: $("#description").val(),
+    genre: [],
+    authors: [],
+  };
+  $("#genre")
+    .val()
+    .split(/,+/)
+    .forEach((element) => {
+      element = element.trim();
+      book.genre.push(element);
+    });
+  $("#authors")
+    .val()
+    .split(/,+/)
+    .forEach((element) => {
+      element = element.trim();
+      book.authors.push(element);
+    });
+  books.push(book);
+}
+
+function emptyList() {
+  $(".table").find("tbody").empty();
+}
+
+function createList() {
+  books.forEach((book) => {
+    $(".table")
+      .find("tbody")
+      .append(
+        `<tr><td>${book.title}</td><td>${
+          book.description
+        }</td><td>${book.genre.join(", ")}</td><td>${book.authors.join(
+          ", "
+        )}</td></tr>`
+      );
+  });
+}
+
+function showList() {
+  $(".table, #clear, #send").removeClass("d-none");
+}
+
+function clearForm() {
+  $("#title, #description, #genre, #authors").val("");
+}
+
+function emptyBooks() {
+  books.length = 0;
+}
+
+function hideList() {
+  $(".table, #clear, #send").addClass("d-none");
+}
+
+function send() {
+  $.post("http://httpbin.org/post", books, function (_data, status) {
+    if (status == "success") {
+      alert(`Conteúdo enviado com sucesso!`);
+    }
+  });
+}
+
+$("#add").click(function () {
+  addBook();
+  emptyList();
+  createList();
+  showList();
+  clearForm();
+});
+
+$("#clear").click(function () {
+  emptyBooks();
+  emptyList();
+  hideList();
+});
+
+$("#send").click(function () {
+  send();
+  emptyBooks();
+  emptyList();
+  hideList();
+  clearForm();
+});
